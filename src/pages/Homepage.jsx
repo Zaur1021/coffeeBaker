68003d1322ddb2a247c9877aa8b34eab9dbf35e9
Zaurbek Shapiev <shapiev.zaur@outlook.com>
2023-08-28 17:41:07 +0800

    Few changes to the homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,6 +7,17 @@ function Homepage() {
         <h1>coffeeBaker</h1>
         <h3>CONTACT US</h3>
       </nav>
+      <main>
+        <h2>
+          <strong>You</strong> enjoy our coffee and bakery
+        </h2>
+        <h2>coffeeBaker keeps track of the quality and low prices</h2>
+        <div>
+          <button>Order</button>
+          <button>Delivery</button>
+        </div>
+      </main>
+      <footer>Copyright &copy; by coffeeBaker CO</footer>
     </div>
   );
 }
